b57ebbd4fa87f4cee74af603e45c1fd41479a307
Alexander <alexanderalbers01@gmail.com>
2019-06-12 13:13:27 +0200

    BIG SERVER UPDATE - Serverfiles neu angepasst - Mehr Struktur etc.
    STRUKTUR
    Externe Request's (app.js wurde einfach zu groß)
    "Datenbank-Abfrag" Funktionen in Database Request (Basis aller Abfragen)
    Test HTML's werden nicht mehr unterstützt
    
    BEARBEITUNGEN
    - Nix funktioniert AUßER! =
    -> Login/Registration
    -> SessionID create + check (Überarbeitet)
    -> Funktionen item.get und item.search hinzugefügt in DatabaseRequestListener
    
    WEITERE FUNKTIONEN UND LOGIN/REGISTRATION + SESSION VERGABE WIRD MORGEN BEARBEITET.

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const mysql = require("mysql");
-const script = require('./functions/script.js');
-const login = require('./functions/login.js');
+const UserListen = require('./listener/UserListener.js');
+const SupportListen = require('./listener/SupportListener.js');
+const DatabaseRequestListen = require('./listener/DatabaseRequestListener.js');
 
 const app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
 
+this.io = require('socket.io')(http);
 this.connection = mysql.createConnection({
     host: "alexkutschera.de",
     user: "seminarkurs",
@@ -19,131 +20,18 @@ this.connection.connect((e) => {
     if (e) throw e;
     console.log('connected');
 });
-
+/*
 this.connection.query('DELETE FROM Sitzungen WHERE Benutzer_Nr > 0', function (e) {
     if (e) throw e;
     console.log('Sessions wurden gelöscht');
 });
-
-
-io.on("connection", (socket) => {
+*/
+this.io.on("connection", (socket) => {
     console.log('Beim Server angekommen (io.on)');
-
-    //Input= Item_ID (durch Scannen) Output= Daten zum Item
-    socket.on('get_item_by_item', function (data) {
-        console.log(data);
-        this.connection.query('SELECT ARTIKEL_ID FROM Item_from_Artikel WHERE ITEM_ID = ?', [data.ITEM_ID], (e, rows) => {
-            if (e) throw e;
-            console.log(rows);
-            this.connection.query('SELECT * FROM Artikel WHERE ARTIKEL_ID = ?', [rows.ARTIKEL_ID], (e, result) => {
-                if (e) throw e;
-                console.log(result);
-                socket.emit('send_item_by_item', result);
-            });
-        });
-    });
-
-    //Nachricht weiterleiten (An alle)
-    socket.on('chat_emit', function (data) {
-        io.sockets.emit('chat_send', data);
-    });
-
-    //Abfrage der Tabellensuche//
-    /* Benötigt data.table */
-    socket.on('ask_table', function (table) {
-        console.log(table);
-
-        //Verarbeitung
-        script.ask_table(table, this.connection).then((result) => {
-            console.log(result);
-
-            //Ergebnisse zurücksenden
-            socket.emit('get_table', result);
-        }).catch((e) => {
-            throw e;
-        });
-    });
-
-    //Löschen der Tabelle/Spalte//
-    /* Benötigt data.delete_table und data.delete_condition */
-    socket.on('delete', function (data) {
-        console.log(data);
-
-        //Verarbeitung
-        script.delete(data, this.connection).then((result) => {
-            console.log(result);
-
-            //Ergebnisse zurücksenden
-            socket.emit('get_delete', result);
-        }).catch((e) => {
-            throw e;
-        });
-    });
-
-    //Einfügen von Daten in Tabelle//
-    /* Benötigt data.insert_table, data.insert_columns und data.insert_values */
-    socket.on('insert', function (data) {
-        console.log("line:78\n" + data);
-
-        //Verarbeitung
-        script.insert(data, this.connection).then((result) => {
-
-            console.log(result);
-            console.log("line:82" + result);
-            //Ergebnisse zurücksenden
-            socket.emit('get_insert', result);
-        }).catch((e) => {
-            throw e;
-        });
-    });
-
-    //REGISTRIERUNG
-    /*  data.reg_BN = Benutzername und data.reg_PW = Passwort */
-    /* Gibt data.result zurück */
-    socket.on('registration', function (data) {
-        console.log(data);
-
-        //Verarbeitung
-        login.setPW(data, this.connection).then((result) => {
-            console.log(result);
-
-            //Ergebnisse zurücksenden
-            socket.emit('get_registration', result);
-        }).catch((e) => {
-            throw e;
-        });
-    });
-
-    //LOGIN
-    /* Benötigt data.log_BN = Benutzername und data.log_PW = Passwort */
-    /* Gibt data.result zurück */
-    socket.on('login', function (data) {
-        console.log(data);
-
-        //Verarbeitung
-        login.getPW(data, this.connection).then((result) => {
-            console.log(result);
-
-
-            //Ergebnisse zurücksenden
-            socket.emit('get_login', result);
-        }).catch((e) => {
-            throw e;
-        });
-
-    });
-
-    //PDF Abfrage
-    /* Benötigt data.artikel_id */
-    /* Gibt data.pdf_link zurück */
-    socket.on('pdf', (data) => {
-        console.log(data);
-        this.connection.query('SELECT PDF_link FROM ARTIKEL WHERE ARTIKEL_ID = ' + data.artikel_id, (e, rows) => {
-            if (e) throw e;
-            socket.emit('pdf', rows);
-        })
-    });
+    UserListen.listenForUser(socket);
+    DatabaseRequestListen.listenForItems(socket);
+    SupportListen.listenForSupport(socket);
 });
-app.use(express.static('public'));
 
+app.use(express.static('public'));
 http.listen(3000);
---
+++ functions/Login.js
@@ -0,0 +1,54 @@
+const bcrypt = require('bcrypt');
+const app = require('../app.js');
+const session = require('./Session.js');
+const saltRounds = 10;
+
+this.getPW = (data) => {
+    return new Promise((resolve, reject) => {
+        let response = {};
+        app.connection.query('SELECT * FROM Benutzer WHERE Benutzername = ?', [data.log_BN], function (e, db_res) {
+            if (e || db_res.length >= 0) reject(e);
+            bcrypt.compare(data.log_PW, db_res[0]['Passwort'], function (err, res) {
+                if (err) reject(e);
+                if (res) {
+                    response.result = "Passwort stimmt überein";
+                    session.generateSessionId(db_res[0]['Benutzer_Nr']).then((res) => {
+                        response.session_id = res;
+                        console.log(response);
+                        resolve(response);
+                    }).catch((e) => {
+                        reject(e);
+                    });
+                } else {
+                    response.result = "Passwort und Benutzername stimmen nicht überein";
+                    console.log(response);
+                    resolve(response);
+                }
+            });
+        });
+    });
+};
+
+this.setPW = (data) => {
+    //Benutzer prüfen
+    return new Promise((resolve, reject) => {
+        app.connection.query('SELECT * FROM Benutzer WHERE Benutzername = "' + data.reg_BN + '"', function (e, rows) {
+            if (e) reject(e);
+            let result = {};
+            if (rows.length === 0) {
+                //Passwort und Benutzer erstellen
+                bcrypt.hash(data.reg_PW, saltRounds, function (err, hash) {
+                    app.connection.query('INSERT INTO Benutzer (Benutzername, Passwort) VALUES(?, ?)', [data.reg_BN, hash], function (e) {
+                        if (e) reject(e);
+                    })
+                });
+                result.result = "Benutzerkonto wurde erstellt";
+
+            } else {
+                //Benutzer schon vergeben
+                result.result = "Benutzername schon vergeben";
+            }
+            resolve(result);
+        });
+    });
+};
